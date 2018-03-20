225315704e877a92e1d5d4e1d2023d10e88393dd
John Tu <tupt@qsoftvietnam.com>
2018-03-20 14:08:52 +0700

    set localhost ip as default ip

--- start.sh
+++ start.sh
@@ -1,6 +1,7 @@
 IMAGE=$1
 : ${IMAGE:=thanhtu/spark}
-PUBLIC_DNS=$(ifconfig | awk '/inet /{print $1}' | grep -v 127.0.0.1 | tail -1)
+# PUBLIC_DNS=$(ifconfig | awk '/inet /{print $2}' | grep -v 127.0.0.1 | tail -1)
+PUBLIC_DNS=127.0.0.1
 IMAGE_CHECK=$(docker images | grep $IMAGE)
 
 if [[ -z $IMAGE_CHECK ]];then
