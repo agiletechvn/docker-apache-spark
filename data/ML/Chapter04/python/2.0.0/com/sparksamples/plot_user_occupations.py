1fbbb1657f0a45c19c219520a0654642a0d155bd
John Tu <tupt@qsoftvietnam.com>
2018-03-26 11:57:42 +0700

    change source code become simpler

--- data/ML/Chapter04/python/2.0.0/com/sparksamples/plot_user_occupations.py
+++ data/ML/Chapter04/python/2.0.0/com/sparksamples/plot_user_occupations.py
@@ -5,21 +5,18 @@ from util import get_user_data
 
 
 def main():
+    # get user data
     user_data = get_user_data()
+    # extract occupation
     user_occ = user_data.groupby("occupation").count().collect()
-
-    user_occ_len = len(user_occ)
-    user_occ_list = []
-    for i in range(0, (user_occ_len - 1)):
-        element = user_occ[i]
-        count = element. __getattr__('count')
-
-        tup = (element.occupation, count)
-        user_occ_list.append(tup)
+    # extract list of tuple occupation by length    
+    # user_occ_list = map(lambda x: (x.occupation, x.__getattr__('count')), user_occ)
+    user_occ_list = [(x.occupation, x.__getattr__('count')) for x in user_occ]
     x_axis1 = np.array([c[0] for c in user_occ_list])
     y_axis1 = np.array([c[1] for c in user_occ_list])
-    x_axis = x_axis1[np.argsort(y_axis1)]
-    y_axis = y_axis1[np.argsort(y_axis1)]
+    sorted_ind = np.argsort(y_axis1)
+    x_axis = x_axis1[sorted_ind]    
+    y_axis = y_axis1[sorted_ind]
 
     pos = np.arange(len(x_axis))
     width = 1.0
